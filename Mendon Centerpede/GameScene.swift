f6645ba05e50d91e8dfc1fa7264fc2c273d93ace
Scott J. Kleper <klep@getpocket.com>
2018-04-13 19:05:41 -0400

    initial work

--- Mendon Centerpede/GameScene.swift
+++ Mendon Centerpede/GameScene.swift
@@ -10,7 +10,77 @@ import SpriteKit
 import GameplayKit
 
 class GameScene: SKScene {
+    private let ship = SKSpriteNode(imageNamed: "ship")
+    private let head = SKSpriteNode(imageNamed: "head")
     
+    override func didMove(to view: SKView) {
+        backgroundColor = SKColor.purple
+        ship.position = CGPoint(x: size.width * 0.1, y: size.height * 0.5)
+        let originalWidth = ship.size.width
+        let width = size.width / 20.0
+        let scale = width / originalWidth
+        let height = ship.size.height * scale
+        ship.size = CGSize(width: width, height: height)
+        addChild(ship)
+        
+        run(SKAction.repeatForever(
+            SKAction.sequence([
+                SKAction.run(addHead),
+                SKAction.wait(forDuration: 1.0)
+                ])
+        ))
+    }
+    
+    func random() -> CGFloat {
+        return CGFloat(Float(arc4random()) / 0xFFFFFFFF)
+    }
+    
+    func random(min: CGFloat, max: CGFloat) -> CGFloat {
+        return random() * (max - min) + min
+    }
+    
+    func addHead() {
+        
+        // Create sprite
+        let head = SKSpriteNode(imageNamed: "head")
+        
+        let originalWidth = head.size.width
+        let width = size.width / 20.0
+        let scale = width / originalWidth
+        let height = head.size.height * scale
+        head.size = CGSize(width: width, height: height)
+        
+        // Determine where to spawn the monster along the Y axis
+        let actualY = random(min: head.size.height/2, max: size.height - head.size.height/2)
+        
+        // Position the monster slightly off-screen along the right edge,
+        // and along a random position along the Y axis as calculated above
+        head.position = CGPoint(x: size.width + head.size.width/2, y: actualY)
+        
+        // Add the monster to the scene
+        addChild(head)
+        
+        // Determine speed of the monster
+        let actualDuration = random(min: CGFloat(2.0), max: CGFloat(4.0))
+        
+        // Create the actions
+        let actionMove = SKAction.move(to: CGPoint(x: -head.size.width/2, y: actualY), duration: TimeInterval(actualDuration))
+        let actionMoveDone = SKAction.removeFromParent()
+        head.run(SKAction.sequence([actionMove, actionMoveDone]))
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        let projectile = SKSpriteNode(imageNamed: "pixel")
+        projectile.position = ship.position
+        projectile.size = CGSize(width: ship.size.width * 0.1, height: ship.size.height * 0.1)
+
+        addChild(projectile)
+        
+        let moveAction = SKAction.move(to: CGPoint(x: projectile.position.x, y: size.height), duration: 2.0)
+        let moveActionDone = SKAction.removeFromParent()
+        projectile.run(SKAction.sequence([moveAction, moveActionDone]))
+    }
+    /*
     private var label : SKLabelNode?
     private var spinnyNode : SKShapeNode?
     
@@ -85,5 +155,5 @@ class GameScene: SKScene {
     
     override func update(_ currentTime: TimeInterval) {
         // Called before each frame is rendered
-    }
+    }*/
 }
